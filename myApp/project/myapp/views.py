17e3928520ae6dd414d4539a615be0f479a9d5da
Warinthorn.pi <85553410+kumA0taku@users.noreply.github.com>
2023-08-25 08:16:02 +0700

    Delete myApp directory

--- myApp/project/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-# Create your views here.
-def index(request):
-    return render(request, 'menu/home.html')
-
-def about(request):
-    return render(request, 'menu/about.html')
-def aidata(request):
-    return render(request, 'menu/ai.html')
